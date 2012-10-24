2e20b49ab6c7932e4fda2100518e2ed9d2750e6d
Tony Lee <github@roganartu.com>
2012-10-24 16:08:44 +1000

    Rename module to ext3301

--- super.c
+++ super.c
@@ -1423,7 +1423,7 @@ out:
 
 static struct file_system_type ext2_fs_type = {
 	.owner		= THIS_MODULE,
-	.name		= "ext2",
+	.name		= "ext3301",
 	.get_sb		= ext2_get_sb,
 	.kill_sb	= kill_block_super,
 	.fs_flags	= FS_REQUIRES_DEV,
